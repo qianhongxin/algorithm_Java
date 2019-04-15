87394ca0e1e63b2ab358de1ca550e07ada668f85
32415 <hongxinqian@ehai.cn>
2019-04-15 11:21:27 +0800

    冒泡排序

--- src/main/java/study/me/algorithm/BubbleSort.java
+++ src/main/java/study/me/algorithm/BubbleSort.java
@@ -3,19 +3,29 @@ package study.me.algorithm;
 public class BubbleSort {
 
     public static void sort(int[] arr) {
-        if(arr == null) {
+        if(arr == null || arr.length == 1) {
             return;
         }
 
         int n = arr.length;
         int tmp;
+        // 每次只排序一个数，相邻元素比较大小。每一趟排序好的一个元素都在最后边一个位置上（从小到大，从大到小都是）。所以每次j都是从0开始往后比较
         for(int i = 0; i < n; i++) {
+            // j的条件是 n-i-1
             for(int j = 0; j < n - i - 1; j++) {
+                // 从小到大
                 if(arr[j] > arr[j + 1]) {
                     tmp = arr[j + 1];
                     arr[j + 1] = arr[j];
                     arr[j] = tmp;
                 }
+
+                // 从大到小
+//                if(arr[j] < arr[j + 1]) {
+//                    tmp = arr[j];
+//                    arr[j] = arr[j + 1];
+//                    arr[j + 1] = tmp;
+//                }
             }
         }
     }
